8400d62cbc7a42b512df4a74407720851f85672f
Andrigo <156147956+fns7k@users.noreply.github.com>
2024-10-02 20:51:15 -0300

    Add files via upload

---
+++ Aula 14/Monitoramento_Veicular/reconhecimento_caracteres.py
@@ -0,0 +1,21 @@
+# C:\Users\Aluno\AppData\Local\Programs\Tesseract-OCR\tesseract.exe
+
+import cv2
+import pytesseract
+
+
+def reconhecer(imagem):
+    # Chamando o executável de reconhecimento de caracteres (biblioteca)
+    pytesseract.pytesseract.tesseract_cmd=r"C:\Users\Aluno\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+
+    #leitura de uma imagem que contém textos
+    placa = cv2.imread(imagem, 0)
+
+    # Conveter/capturar textos da imagem
+    meusTextos = pytesseract.image_to_string(placa, config='1 eng --oem 3 --psm 12')
+
+    print(meusTextos)
+    return(meusTextos)
+
+# reconhecer('livros.png')
+# Enviar uma imagem para a função e exibir os textos reconhecidos
